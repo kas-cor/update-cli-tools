8284cbe0ca152276aac116a5cc2172683b117129
Alexsander <kas-cor@users.noreply.github.com>
2025-09-26 12:21:22 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,12 +18,12 @@ for tool_info in "${tools[@]}"; do
   IFS=':' read -r exe_name update_cmd <<< "$tool_info"
 
   if command -v "$exe_name" &> /dev/null; then
-    echo "Found $exe_name. Attempting to update..."
+    echo -n "Found $exe_name. Attempting to update..."
     $update_cmd &> /dev/null
     if [ $? -eq 0 ]; then
-      echo "The update of $exe_name was successful (or not required)."
+      echo "Successful (or not required)."
     else
-      echo "Error updating $exe_name or update command not implemented."
+      echo "Error or update command not implemented."
     fi
   fi
 done
